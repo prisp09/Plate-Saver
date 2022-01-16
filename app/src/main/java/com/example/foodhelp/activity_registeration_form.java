52a68e5c09d9255b684030048fb8107b21bf27e2
laibaz16 <laibazahid16@gmail.com>
2022-01-16 11:04:59 -0500

    changes

--- app/src/main/java/com/example/foodhelp/activity_registeration_form.java
+++ app/src/main/java/com/example/foodhelp/activity_registeration_form.java
@@ -2,7 +2,10 @@ package com.example.foodhelp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class activity_registeration_form extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class activity_registeration_form extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registeration_form);
+
+        //leads to completion of registeration
+        Button Register_Button = (Button) findViewById(R.id.register);
+        Register_Button.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                startActivity(new Intent(activity_registeration_form.this, register_Note.class));
+            }
+        });
     }
 }
